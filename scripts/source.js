249ed28ff6e07081f8428346711e656ae3a51ea7
Jane Smith <example@email.com>
2017-03-07 23:56:27 -0800

    completed HTML for projects and attempted to render them on the screen

---
+++ scripts/source.js
@@ -0,0 +1,33 @@
+'use strict';
+var ivy_projects = [
+  {
+    title:'72 RPM',
+    date:'March 2017',
+    screenshot: 'vendor/screenshots/game_page.png',
+    link:'https://72rpm.github.io/project/index.html',
+    goal: 'Build an app with a team',
+    type: 'Collaborative team project | CodeFellows',
+    technology:'HTML, CSS, Javascript, JSON, Local Storage'
+  },
+
+  {
+    title:'BusMall',
+    date_completed:'February 2017',
+    screenshot:['vendor/screenshots/busMall_main.png','vendor/screenshots/chart.png'],
+    link:'https://lilaivy.github.io/bus_mall/',
+    goal:'Collect & present market research for magazine launch', //Local Storage, eventHandlers
+    type: 'Solo project | CodeFellows',
+    technology:'HTML, CSS, Javascript, Charts.JS'
+  },
+
+  {
+    title:'Seattle Cookies',
+    date_completed:'February 2017',
+    screenshot:['vendor/screenshots/busMall_main.png','vendor/screenshots/chart.png'], //replace with proper screenshot
+    link:'https://lilaivy.github.io/salmon_cookies/',
+    goal:'Design & develop cookie store web site & present sales data to biz admin', //DOM manipulation
+    type: 'Solo project | CodeFellows',
+    technology:'HTML, CSS, Javascript'
+  },
+
+];
