eb1c65b251059c5add7fe34ddb2cbb38c4eaa854
Jane Smith <example@email.com>
2017-03-14 15:16:34 -0700

    got it working.  I am going to add a public folder to minimize data sent during requests

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('./portfolio'))
+app.use(express.static('.'))
 app.get('*',function(request, response) {
-    response.sendFile('/portfolio.html'), {root: '.'})
+    response.sendFile('/portfolio/index.html', {root: '.'})
 });
 
 app.listen(PORT, function(){
