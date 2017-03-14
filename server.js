8d786a2e3eac5a59d403ca8947375ba2e926622f
Jane Smith <example@email.com>
2017-03-14 15:24:31 -0700

    added public file

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('.'))
+app.use(express.static('./public'))
 app.get('*',function(request, response) {
-    response.sendFile('/portfolio/index.html', {root: '.'})
+    response.sendFile('/portfolio/public/index.html', {root: '.'})
 });
 
 app.listen(PORT, function(){
