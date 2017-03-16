6b6b4df5860a80fa709e78b468cbe007829b4334
Jane Smith <example@email.com>
2017-03-15 17:02:51 -0700

    got local storage working

--- scripts/app.js
+++ scripts/app.js
@@ -27,53 +27,38 @@ function mainNavHandler() {
   });
   $('.main-nav .tab:first').click();
 }
+mainNavHandler();
 
-// Push my projects into an array
 
-//creating a variable for my function that pushes my projects into an array. I'm getting these projects from the results of my AJAX call. It should be an array of objects.
 Projects.loadAll = function (project_data) {
   project_data.forEach(function(ele) {
     Projects.all.push(new Projects(ele));
   });
 
-  //gave a name to my function that renders my projects to HTML
-  var initIndexPage = function() {
-    Projects.all.forEach(function(project){
-      $('#render_template').append(project.toHtml());
-    });
-
-    mainNavHandler();
-  };
+ 
+  Projects.all.forEach(function(p){
+    $('#render_template').append(p.toHtml());
+  });
+  
 };
 
 Projects.fetchAll = function() {
-  //This part of the function tells the program what to do if there is anything in LocalStorage.  I do no understand where rawData came from in the lab code. I assumed it was a property on localStorage
-    if (localStorage.rawData) {
-    //I'm parsing out the stringified JSON data so that it returns to me as an object and storing it on a variable called parsedData.  Again, I assumed that rawData was a property on localStorage, I am not clear about what rawData represents
-      var parsedData = JSON.parse(localStorage.rawData);
-    //I'm calling the function Projects.loadAll and passing it the paredData
-      Projects.loadAll(parsedData);
-    // initIndexPage();
-    }else{
-    //if there is nothing in LocalStorage, I'm making an ajax call to retrieve my source data from the server
-      $.ajax({
-      //this is the path to the source data.  I'm making the request here.
-        url: 'data/source.json',
-      //I'm using the GET method to make this request.
-        method:'GET',
-      //I'm writing a function with the parameter of data
-        success: function(data){
-        //I'm stringifying data and storing that stringified data on the variable rawDataJSON
-          var rawDataJSON = JSON.stringify(data);
-        //I'm setting the items on local storage and giving the variable rawData the valie of rawDataJSON
-          localStorage.setItem('rawData', rawDataJSON);
-        //Should I be calling loadAll here?
-          Projects.loadAll(data);
-          initIndexPage();
+  if (localStorage.rawData) {
+    var parsedData = JSON.parse(localStorage.rawData);
+    Projects.loadAll(parsedData);
+  }else{
+    $.ajax({
+      url: 'data/source.json',
+      method:'GET',
+      success: function(data){
+        localStorage.setItem('rawData', JSON.stringify(data));
+        Projects.loadAll(data);
+      }, 
+      error: function(err){
+        console.log('in error handler', err);
+      }
+    }); 
+  }
 
-        }, 
-        error: function(err){
-          console.log('in error handler', err);
-        }
-      }); 
-  };
+};
+
